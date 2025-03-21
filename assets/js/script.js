7146572176a730be2d9a5b4940f4dc277b80c46d
sambilheimer <sambilheimer@gmail.com>
2025-03-21 12:00:39 -0400

    .

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// This file is for any custom JavaScript you might want to add
+// For a simple blog, you may not need any JS at all
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Code to execute when the DOM is fully loaded
+  console.log("Blog loaded successfully!");
+});
